df050928ba4659c47ecb86e993d8704a4c50c32c
A1eksX <A1kon1.618@yandex.ru>
2023-11-23 23:51:02 +0300

    Seminar2

--- SeminarTwo/src/main/java/Task1.java
+++ SeminarTwo/src/main/java/Task1.java
@@ -1,2 +1,22 @@
+/**
+ * Задача
+ * Реализовать чисто структурную реализацию вычисления следа для любой матрицы NxN
+ *
+ * След матрицы - это сумма чисел на её главной диагонали. След определён только для квадратных матриц
+ * (количество столбцов = количеству строк).
+ */
 public class Task1 {
+    public static void main(String[] args) {
+        int [][] matrix = {{1,2,3}, {4, 5, 6}, {7, 8, 9}};
+
+        System.out.println(task1(matrix));
+    }
+
+    private static int task1(int [][] array2D) {
+        int summ = 0;
+        for (int i = 0; i < array2D.length; i++) {
+            summ += array2D[i][i];
+        }
+        return summ;
+    }
 }
---
+++ SeminarTwo/src/main/java/Task2.java
@@ -0,0 +1,36 @@
+/**
+ * На вход подается число в десятичной системе счисления
+ * Задача
+ * Написать скрипт в любой парадигме, который возвращает полученное число переведенное в двоичную
+ * систему счисления. Обоснуйте сделанный выбор парадигм.
+ */
+public class Task2 {
+    public static void main(String[] args) {
+        int n = 1000;
+        int base = 16;
+        System.out.println(task2(n, base));
+
+    }
+
+    private static String task2(int n, int base) {
+        int remains;
+        StringBuilder builder = new StringBuilder();
+
+        while (n > 0){
+            remains = n % base;
+            if (remains < 9){
+                builder.append(remains);
+            }
+            if(remains > 9){ // 14 - E
+                remains = (char)(55 + remains); //(char)(65) - согласно табл ASCII это символ A. В данном примере если
+                // остаток от деления больше 9 (например 14), то согласно табл ASCII - 14 - это 55 + 14 = 69 - т.е. Е
+                // т.к. символы используемые в 16-тиричном формате идут 0 1 2 3 4 5 6 7 8 9 A B C D E F, то чтобы
+                // получить А, надо из 65 - 10. отсюда вылезает 55
+                char ch = (char) remains;
+                builder.append(ch);
+            }
+            n /= base;
+        }
+        return String.valueOf(builder.reverse());
+    }
+}
